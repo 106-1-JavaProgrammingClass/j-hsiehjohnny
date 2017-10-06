5a383f53ea6fe55eae5c0b6ffac4de30301bc0e7
hsiehjohnny <32057008+hsiehjohnny@users.noreply.github.com>
2017-10-06 15:23:02 +0800

    546416

--- src/HW12.java
+++ src/HW12.java
@@ -5,6 +5,7 @@ public class HW12 {
         Scanner scn = new Scanner(System.in);
         double v1 = scn.nextDouble();
         int v2 = scn.nextInt();
-        System.out.println();
+        double v3 = Math.pow(10,v2);
+        System.out.println(Math.round(v1*v3)/v3);
     }
 }
